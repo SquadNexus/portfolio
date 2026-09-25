8219c64d38707e08d05f7cdb663a2b8cf8d9719a
SquadGameDealer <iamsquardii@gmail.com>
2026-09-25 20:05:16 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,34 @@
+// Typing Text Effect for Hero Section
+const typedTextSpan = document.querySelector(".typed-text");
+const textArray = ["Web Developer", "Computer Engineering Student", "Tech Enthusiast"];
+const typingDelay = 100;
+const erasingDelay = 50;
+const newTextDelay = 2000; // Delay between current and next text
+let textArrayIndex = 0;
+let charIndex = 0;
+
+function type() {
+    if (charIndex < textArray[textArrayIndex].length) {
+        typedTextSpan.textContent += textArray[textArrayIndex].charAt(charIndex);
+        charIndex++;
+        setTimeout(type, typingDelay);
+    } else {
+        setTimeout(erase, newTextDelay);
+    }
+}
+
+function erase() {
+    if (charIndex > 0) {
+        typedTextSpan.textContent = textArray[textArrayIndex].substring(0, charIndex - 1);
+        charIndex--;
+        setTimeout(erase, erasingDelay);
+    } else {
+        textArrayIndex++;
+        if (textArrayIndex >= textArray.length) textArrayIndex = 0;
+        setTimeout(type, typingDelay + 1100);
+    }
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    if (textArray.length) setTimeout(type, newTextDelay + 250);
+});
